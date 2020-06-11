85ba8d8d810a342b38f4a80c0c50f59c38c771e2
Lex Bosch <31917165+LexBosch@users.noreply.github.com>
2020-06-11 16:12:37 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+from distutils.core import setup
+setup(
+  name = 'wijnen',       
+  packages = ['wijnen'],  
+  version = '0.1',     
+  license='MIT',       
+  description = 'Client to connect to the wijnen web-api',  
+  author = 'Lex Bosch',                  
+  author_email = 'lexbosch@live.nl',     
+  url = 'https://github.com/LexBosch/wijnen-client', 
+  download_url = 'https://github.com/LexBosch/wijnen-client/archive/0.1.tar.gz',   
+  keywords = ['wijnen', 'api', 'client'],   
+  install_requires=[            
+          'concurrent',
+          'requests',
+          'json',
+          'hashlib',  
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+
+
+],
+)
